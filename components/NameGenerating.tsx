57b415351e5257fd3ec183dbab4ff3b3e51cb5b3
noreplydev <contact.cristiansanchez@gmail.com>
2024-03-04 01:17:03 +0100

    [FT] string generation component

--- components/NameGenerating.tsx
+++ components/NameGenerating.tsx
@@ -2,25 +2,52 @@
 import { useEffect, useState } from 'react';
 
 export function NameGenerating() {
+  const RESOLVED_STRING = "@noreplydev"
+  const CHARACTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"$%&/()=?¿*';
+
+  const RESOLVED_STRING_LENGTH = RESOLVED_STRING.length
+  const MILISECONDS_TO_RESOLV = 3000
+  const MILISECONDS_PER_ITERATIONS = Math.floor(MILISECONDS_TO_RESOLV / RESOLVED_STRING_LENGTH)
+
+  const [prefix, setPrefix] = useState("")
   const [text, setText] = useState("01001110110")
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!"$%&/()=?¿*';
+  let iterationsCounter = 0
+
+  const updatePrefix = () => {
+    const _prefix = RESOLVED_STRING.slice(0, iterationsCounter)
+    setPrefix(_prefix)
+  }
 
   useEffect(() => {
     const interval = setInterval(() => {
-      const newChars = text.split('').map(() => {
-        const randomIndex = Math.floor(Math.random() * characters.length)
-        return characters[randomIndex]
+      console.log('iterations', iterationsCounter)
+      const newChars = text.split('').slice(iterationsCounter).map(() => {
+        const randomIndex = Math.floor(Math.random() * CHARACTERS.length)
+        return CHARACTERS[randomIndex]
       })
       setText(newChars.join(''))
+      updatePrefix()
     }, 50)
 
+    const revealInterval = setInterval(() => {
+      updatePrefix()
+      iterationsCounter++
+    }, MILISECONDS_PER_ITERATIONS)
+
     setTimeout(() => {
+      clearInterval(revealInterval)
       clearInterval(interval)
-      setText("@noreplydev")
-    }, 3000)
+      setText('')
+      setPrefix(RESOLVED_STRING)
+    }, MILISECONDS_TO_RESOLV)
+
+    return () => {
+      clearInterval(revealInterval)
+      clearInterval(interval)
+    }
   }, [])
 
   return <p
     className={`text-2xl font-thin text-[#949494] text-center`}
-  >{text}</p>
+  >{prefix}{text}</p>
 }
