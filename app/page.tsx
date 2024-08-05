9b92f3105f758f90042e1a2f4cc6024083a40e54
noreplydev <contact.cristiansanchez@gmail.com>
2024-08-06 01:33:21 +0200

    Add background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { GeneratedString } from '@/components/GeneratedString'
-import { getPosts, getRecentsPosts } from '@/lib/fs'
 
 export default async function Home() {
   return (
@@ -7,7 +6,12 @@ export default async function Home() {
       className="relative flex h-screen w-screen flex-col items-center justify-center overflow-scroll 
         gap-8 md:px-[26vw]"
     >
+      <div style={{
+        position: "absolute", height: "100%", width: "100%",
+        backgroundImage: "url(/noise.png)", backgroundRepeat: "repeat",
+        backgroundSize: "10%", opacity: "0.3",
+      }}></div>
       <GeneratedString resolvedString='noreplydev' />
-    </main>
+    </ main>
   )
 }
