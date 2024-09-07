787a497acbe28c80e671a18610d992a2155fb98f
noreplydev <contact.cristiansanchez@gmail.com>
2024-09-07 18:51:45 +0200

    it is time for change

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { GeneratedString } from '@/components/GeneratedString'
+import Logo from '@/components/Logo'
 
 export default async function Home() {
   return (
@@ -6,15 +7,7 @@ export default async function Home() {
       className="relative flex h-screen w-screen flex-col items-center justify-center
         gap-8 md:px-[26vw] overflow-hidden"
     >
-      <div style={{
-        position: "absolute", height: "100%", width: "100%",
-        backgroundImage: "url(/noise.png)", backgroundRepeat: "repeat",
-        backgroundSize: "10%", opacity: "0.3",
-      }}></div>
-      <img src="/noreplydev_logo.png" style={{
-        height: "10vh", width: "auto",
-      }} />
-      <GeneratedString resolvedString='noreplydev' />
+      <Logo />
     </ main>
   )
 }
